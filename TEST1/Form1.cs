8a46683c4fad7f7874e3d8f5df9920db05021144
mariomartinez97 <m4mariomiguel@gmail.com>
2017-08-17 01:33:58 -0400

    Initial commit -Parser working -Data into dic -Test with grid view

---
+++ TEST1/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TEST1
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, Word> diccionario;
+        public Form1()
+        {
+            InitializeComponent();
+
+            diccionario = new Dictionary<string, Word>();
+
+
+            StreamReader reader = new StreamReader("short_file2.txt");
+
+            while (!reader.EndOfStream)
+            {
+                Word mWord = new Word();
+                string wordInfo = reader.ReadLine();
+                string[] split = wordInfo.Split('|');
+                for (int i = 0; i < split.Length; i++)
+                {
+                    split[i] = split[i].Remove(0, 1);
+                    split[i] = split[i].Remove(split[i].Length - 1, 1);
+                }
+
+                mWord.palabra = split[0];
+                mWord.traduccion = split[1];
+
+                diccionario.Add(mWord.palabra, mWord);                            
+            }
+
+            foreach (var kvp in diccionario)
+            {                                
+                dataGridView1.Rows.Add(diccionario[kvp.Key].palabra, diccionario[kvp.Key].traduccion);
+            }
+
+            
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+    }
+}
